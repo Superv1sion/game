7776c1903440457a8cde7b1b983d6aa0683c962c
e.odinec <e.odinec@o3.ua>
2017-03-20 17:31:18 +0200

    add config folder. Move webpack config to config

---
+++ config/config.js
@@ -0,0 +1,7 @@
+let config = {
+    app: {
+        port: 3000
+    }
+};
+
+export default config;
--- server.dev.js
+++ server.dev.js
@@ -1,5 +1,21 @@
-let http = require("http");
-import app from './app/app';
+import http from 'http';
+import webpack from 'webpack';
+import webpackDevMiddleware from 'webpack-dev-middleware';
+import webpackHotMiddleware from 'webpack-hot-middleware';
+import path from 'path';
+import express from 'express';
+import webpackConfig from './config/webpack.config';
+
+let app = new express();
+let compiler = webpack(webpackConfig);
+
+app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: webpackConfig.output.publicPath }));
+app.use(webpackHotMiddleware(compiler));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname + '/../public/index.html'));
+});
 
 let server = http.createServer(app);
 
